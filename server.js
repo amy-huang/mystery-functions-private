a11b3a983962a283c233478f0ebbba2cf0496d52
amy-huang <amy.jiamei.huang@gmail.com>
2019-10-15 18:41:16 -0400

    still goin

--- server.js
+++ server.js
@@ -10,14 +10,11 @@ const conPool = new Pool({
 });
 conPool.query('create table if not exists actions ( userID varchar (255), actionID integer, actionType varchar (255), time timestamp, input varchar (255), output varchar (255), result varchar (255), reason varchar (255) );', (err, result) => {
 });
-conPool.query('create table if not exists actions ( userID varchar (255), actionID integer, actionType varchar (255), time timestamp, input varchar (255), output varchar (255), result varchar (255), reason varchar (255) );', (err, result) => {
-});
 
 // This test query works.
-
-// pool.query(`insert into actions (userID) values ('test');`, (err, result) => {
-//   res.status(201).json({ status: 'success', message: 'test row inserted.' })
-// });
+conPool.query(`insert into actions (userID) values ('test');`, (err, result) => {
+  res.status(201).json({ status: 'success', message: 'test row inserted.' })
+});
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,6 +24,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // TODO: make this generic for any input, output types we use: no special charas
 function toDbString(a_list) {
+  if (a_list === undefined) {
+    return ""
+  }
+
   as_str = ""
   for (var i = 0; i < a_list.length; i++) {
     if (as_str.length > 0) {
@@ -45,13 +46,14 @@ app.post('/api/store', (req, res) => {
   var action = req.body
   var id = req.connection.remoteAddress
   var time = action.time
+  var type = action.type
   // For datetime, use yyy-mm-dd hh:mi:ss formatting
 
-  console.log("action: ", action)
-  console.log("key: ", action.key)
-  console.log("in: ", action.in)
-  console.log("time: ", time)
-  console.log("id: ", id)
+  // console.log("action: ", action)
+  // console.log("key: ", action.key)
+  // console.log("in: ", action.in)
+  // console.log("time: ", time)
+  // console.log("id: ", id)
 
   // var client = new Client({
   //   connectionString: process.env.DATABASE_URL,
@@ -59,28 +61,27 @@ app.post('/api/store', (req, res) => {
   // });
   // client.connect()
 
-  if (action.type === "eval_input") {
+  if (type === "eval_input") {
     in_str = toDbString(action.in)
-    console.log("in str: ", in_str)
+    // console.log("in str: ", in_str)
     conPool.query(`insert into actions (userID, actionID, actionType, time, input) values ('$1', '$2', '$3', '$4', '$5');`, [id, action.key, action.type, time, in_str], (err, result) => {
       if (!err) {
-        res.status(201).json({ status: 'success', message: 'final_answer row inserted' })
+        res.send(`Success!`)
       }
     });
-
-  } else if (action.type === "eval_pair") {
+  } else if (type === "eval_pair") {
     in_str = toDbString(action.in)
     out_str = toDbString(action.out)
-
+    // console.log("in str: ", in_str)
     conPool.query(`insert into actions (userID, actionID, actionType, time, input, output, result) values ('$1', '$2', '$3', '$4', '$5', '$6', '$7');`, [id, action.key, action.type, time, in_str, out_str, action.result], (err, result) => {
       if (!err) {
-        res.status(201).json({ status: 'success', message: 'final_answer row inserted' })
+        res.send(`Success!`)
       }
     });
-  } else if (action.type === "final_answer") {
+  } else if (type === "final_answer") {
     conPool.query(`insert into actions (userID, actionID, actionType, time, reason) values ('$1', '$2', '$3', '$4');`, [id, action.key, action.type, time, action.reason], (err, result) => {
       if (!err) {
-        res.status(201).json({ status: 'success', message: 'final_answer row inserted' })
+        res.send(`Success!`)
       }
     });
   }
